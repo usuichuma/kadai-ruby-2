683219f7386564a43a28cc1428090766930441a1
usui.chuma <usuichuma@icloud.com>
2021-09-17 05:41:24 +0000

    1

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,14 @@
+ def fizzbuzz(num)
+      if num % 15 == 0
+          puts "FizzBuzz"
+      elsif num % 3 == 0
+          puts "Fizz"
+      elsif num % 5 == 0
+          puts "Buzz"
+      else
+          puts num
+      end end
+num_max = 100
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
